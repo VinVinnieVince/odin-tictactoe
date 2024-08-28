205c7730fad4fe33fae7f5677a9cabb0a0532031
VinVinnieVince <vincent.chan@hotmail.co.uk>
2024-08-28 17:19:07 +0100

    Add code to input player details
     - Add factory function createPlayer(), returns player info
     - Add temporary prompts for naming players
     - Add IIFE Playermarkers() gives choice for player 1, then auto-assigns
     marker for player 2. If choice is not satisfactory, confirm prompt
     allows user to choose marker again.

--- script/tictactoe.js
+++ script/tictactoe.js
@@ -53,7 +53,7 @@ const Gameboard = ( () => {
             alert('Player has won!');
             return;
         }
-
+        
         win = true;
         for (let i = 0; i < 3; i++) {
             if (grid[row][column] !== grid[i][2 - i]) {
@@ -75,9 +75,76 @@ const Gameboard = ( () => {
         }
     }
 
+    function createPlayer (name, marker) {
+        const score = 0;
+        let playerName = name;
+    
+        function addToScore () {
+            score++;
+        }
+
+        function rename () {
+            playerName = prompt(`Write a new name for Player ${playerName}`);
+        }
+
+        return {
+            playerName,
+            marker,
+            addToScore,
+            rename,
+        }
+    }
+    
+    let playerOneName = prompt('Write a name for Player 1', 'Player 1');
+    let playerTwoName = prompt('Write a name for Player 2', 'Player 2');
+    
+    let playerOneMarker;
+    let playerTwoMarker;
+    const PlayerMarkers = (function assign() {
+        playerOneMarker = prompt(`X or O? (Type 'rand' for random)`);
+    
+        const acceptedInputs = ['x', 'o', 'rand'];
+    
+        while ( !(acceptedInputs.includes(playerOneMarker.toLowerCase())) ) {
+            playerOneMarker = prompt(`Please type in 'x', 'o' or 'rand'!`);
+        }
+    
+        if (playerOneMarker === 'rand') {
+            const randomValue = Math.random() * 2;
+            if (randomValue <= 1) {
+                playerOneMarker = 'o';
+            } else if (randomValue > 1) {
+                playerOneMarker = 'x';
+            } else {
+                console.log('random marker error');
+            }
+            console.log(randomValue, playerOneMarker);
+        }
+    
+        let playerTwoMarker;
+        switch (playerOneMarker) {
+            case 'x':
+                playerTwoMarker = 'o';
+                break;
+            case 'o':
+                playerTwoMarker = 'x';
+                break;
+        }
+        
+        if (! (confirm(`${playerTwoName}'s marker will be ${playerTwoMarker}. Proceed?`))) {
+            assign();
+        }
+    
+        return { assign };
+    })()
+    
+    const PlayerOne = createPlayer(playerOneName, playerOneMarker);
+    const PlayerTwo = createPlayer(playerTwoName, playerTwoMarker);
+
     return {
         grid,
         markGrid,
         reset,
     }
-})()
+})() 
+
